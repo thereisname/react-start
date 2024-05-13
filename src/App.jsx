318d276572c9de144e97f0614523557c38bf1bec
thereisname <happycm1210@naver.com>
2024-05-14 00:04:36 +0900

    확인 취소 버튼 동작 구동

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,32 @@ const initMembers = [
   },
 ]
 
+const InputComponent = ({ targetMember, setClicked, onChange }) => {
+  // targetMember는 클릭된 item을 가져옴.
+  // targetMember중 desc의 값이 변경되고 '확인' 버튼을 눌렀다면 input내 값으로 변경하는 작업.
+  // '취소' 버튼을 클릭할 경우 input에 변경한 값이 존재하더라도 기존 값을 출력함.
+  const [inprogress, setInprogress] = useState(targetMember)
+  return (
+    <div>
+      <input
+        value={inprogress.desc}
+        onChange={(e) => {
+          setInprogress({ ...inprogress, desc: e.target.value })
+        }}
+      />
+      <button
+        onClick={() => {
+          setClicked((prev) => !prev)
+          onChange(inprogress)
+        }}
+      >
+        확인
+      </button>
+      <button onClick={() => setClicked((prev) => !prev)}>취소</button>
+    </div>
+  )
+}
+
 function ItemComponent({ children, member, onChange }) {
   const [clicked, setClicked] = useState(false)
   // children: react에서 제공하는 예약어
@@ -32,28 +58,7 @@ function ItemComponent({ children, member, onChange }) {
     <li>
       <div>{children}</div>
       {clicked ? (
-        <div>
-          <input
-            value={member.desc}
-            onChange={(e) => {
-              onChange({ ...member, desc: e.target.value })
-            }}
-          />
-          <button
-            onClick={() => {
-              setClicked(!clicked)
-            }}
-          >
-            확인
-          </button>
-          <button
-            onClick={() => {
-              setClicked(!clicked)
-            }}
-          >
-            취소
-          </button>
-        </div>
+        <InputComponent targetMember={member} setClicked={setClicked} onChange={onChange} />
       ) : (
         <div onClick={() => setClicked(!clicked)}>
           <div>{member.desc}</div>
