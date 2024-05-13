338312794bfbad91c34649266829cff8ff969929
thereisname <happycm1210@naver.com>
2024-05-13 16:33:30 +0900

    state propy ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
 import './App.css'
+
+// function MyFirstReactComponent(props) {
+//   // const [captured, setCaptured] = useState(props.count)
+
+//   // [props.count]의 값이 변하면 setCaptured()로 값을 변경시킴.
+//   /*
+//     useEffect에서 파라미터는 함수와, array가 존재하는데
+//     array의 값이 바뀐다면(상태 변경) 함수를 실행시켜라는 의미로
+//     여기서는 값을 변경한다.
+
+//     아래 주석 처리된 코드는 가능한 이런 방식으로 사용하지말것!
+//     */
+
+//   // useEffect(() => {
+//   //   setCaptured(props.count)
+//   // }, [props.count])
+
+//   return (
+//     <div>
+//       우와 행복하다!: {props.name} - {props.count}
+//       <button
+//         onClick={() => {
+//           props.setCount(props.count + 1)
+//         }}
+//       >
+//         {'나도 부모님의 상태 바꿀래!'}
+//       </button>
+//     </div>
+//   )
+// }
 
 function App() {
   const [count, setCount] = useState(0)
+  const [string, setString] = useState('')
+  const stringRef = useRef()
+
+  console.log('rerendered')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      {/* <MyFirstReactComponent name='Aaron' count={count} setCount={setCount} /> */}
+      <button
+        onClick={() => {
+          console.log(stringRef.current.value)
+        }}
+      >
+        Ref 안에 무엇이 있을까?
+      </button>
+      <input
+        type='text'
+        ref={stringRef}
+        // value={string}
+        // onChange={(e) => {
+        //   setString(e.target.value)
+        // }}
+      />
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
