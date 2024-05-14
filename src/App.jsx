5eb4ecc0c3de3882d914cea45385969867fabdaf
thereisname <happycm1210@naver.com>
2024-05-14 11:06:35 +0900

    주석 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
 import { useRef, useState } from 'react'
 import './App.css'
 
-// function MyFirstReactComponent(props) {
-//   // const [captured, setCaptured] = useState(props.count)
+function MyFirstReactComponent(props) {
+  // const [captured, setCaptured] = useState(props.count)
 
-//   // [props.count]의 값이 변하면 setCaptured()로 값을 변경시킴.
-//   /*
-//     useEffect에서 파라미터는 함수와, array가 존재하는데
-//     array의 값이 바뀐다면(상태 변경) 함수를 실행시켜라는 의미로
-//     여기서는 값을 변경한다.
+  // [props.count]의 값이 변하면 setCaptured()로 값을 변경시킴.
+  /*
+    useEffect에서 파라미터는 함수와, array가 존재하는데
+    array의 값이 바뀐다면(상태 변경) 함수를 실행시켜라는 의미로
+    여기서는 값을 변경한다.
 
-//     아래 주석 처리된 코드는 가능한 이런 방식으로 사용하지말것!
-//     */
+    아래 주석 처리된 코드는 가능한 이런 방식으로 사용하지말것!
+  */
 
-//   // useEffect(() => {
-//   //   setCaptured(props.count)
-//   // }, [props.count])
+  // useEffect(() => {
+  //   setCaptured(props.count)
+  // }, [props.count])
 
-//   return (
-//     <div>
-//       우와 행복하다!: {props.name} - {props.count}
-//       <button
-//         onClick={() => {
-//           props.setCount(props.count + 1)
-//         }}
-//       >
-//         {'나도 부모님의 상태 바꿀래!'}
-//       </button>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      우와 행복하다!: {props.name} - {props.count}
+      <button
+        onClick={() => {
+          props.setCount(props.count + 1)
+        }}
+      >
+        {'나도 부모님의 상태 바꿀래!'}
+      </button>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
-  const [string, setString] = useState('')
+  // const [string, setString] = useState('')
   const stringRef = useRef()
 
   console.log('rerendered')
 
   return (
     <>
-      {/* <MyFirstReactComponent name='Aaron' count={count} setCount={setCount} /> */}
+      <MyFirstReactComponent name='Aaron' count={count} setCount={setCount} />
       <button
         onClick={() => {
           console.log(stringRef.current.value)
@@ -56,14 +56,6 @@ function App() {
         //   setString(e.target.value)
         // }}
       />
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
